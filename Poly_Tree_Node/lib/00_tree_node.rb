d772b8d241ce98c9f6a7d0d7f190a4a21d6a9bc7
App Academy Student <thomasnawabi@gmail.com>
2017-07-07 15:05:02 -0400

    Added bfs and dfs methods

--- Poly_Tree_Node/lib/00_tree_node.rb
+++ Poly_Tree_Node/lib/00_tree_node.rb
@@ -42,4 +42,26 @@ class PolyTreeNode
 
  end
 
+ def dfs(target_value)
+   return self if self.value == target_value
+
+   self.children.each do |child|
+     result = child.dfs(target_value)
+     return result unless result.nil?
+   end
+
+   nil
+ end
+
+ def bfs(target_value)
+   queue = [self]
+   until queue.empty?
+     current_node = queue.shift
+     return current_node if current_node.value == target_value
+     queue += current_node.children
+   end
+
+   nil
+ end
+
 end
