543c5fd6ef5de3ee69bea28d76bf2e53bfc74df7
App Academy Student <thomasnawabi@gmail.com>
2017-07-07 14:26:17 -0400

    Finished Phase 3

--- Poly_Tree_Node/lib/00_tree_node.rb
+++ Poly_Tree_Node/lib/00_tree_node.rb
@@ -27,6 +27,19 @@ class PolyTreeNode
      parent.children << self
    end
  end
- 
+
+ def add_child(child_node)
+   child_node.parent = self
+ end
+
+ def remove_child(child_node)
+
+   if @children.include?(child_node)
+     child_node.parent = nil
+   else
+     raise "Error - not my child!"
+   end
+
+ end
 
 end
