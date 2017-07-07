372f856bf505dcf3d462282b21f10035d2848dc9
App Academy Student <thomasnawabi@gmail.com>
2017-07-07 12:33:53 -0400

    Add Node project

---
+++ skeleton/lib/00_tree_node.rb
@@ -0,0 +1,31 @@
+class PolyTreeNode
+ def initialize(value)
+   @value = value
+   @parent = nil
+   @children = []
+ end
+
+ def parent
+   @parent
+ end
+
+ def children
+   @children
+ end
+
+ def value
+   @value
+ end
+
+ def parent=(parent)
+  unless @parent.nil?
+    self.parent.children.delete(self)
+  end
+
+  @parent = parent
+  unless parent.nil? || parent.children.include?(self)
+    parent.children << self
+  end
+end
+
+end
